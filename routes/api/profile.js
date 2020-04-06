6f0b991d6ce508e4beb577a8c63b56030646ff47
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-04-06 17:06:54 +0100

    route files created with express router

---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/profile
+// @desc   Test route
+// @access Public
+router.get("/", (req, res) => res.send("User profile"));
+
+module.exports = router;
