05acb8fa065e07dc9f01273f2b96c1dfc60bd9e0
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-04-07 22:13:54 +0100

    Profile model created

---
+++ models/Profile.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  planet: {
+    type: String,
+  },
+  dreamAnimal: {
+    type: String,
+  },
+  dreamColour: {
+    type: String,
+  },
+  dreamMusic: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+  },
+  bio: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
