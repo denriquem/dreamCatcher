6e851084e87a1056dd4174bccd106b4759d45019
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-04-21 12:59:58 +0100

    test: passing tests and renderin correctly

---
+++ client/src/components/auth/Login.test.js
@@ -0,0 +1,30 @@
+import Login from "./Login";
+import Enzyme from "enzyme";
+import { setup, findByTestAttr } from "./../../testHelpers";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let wrapper;
+let loginComponent;
+
+beforeEach(() => {
+  wrapper = setup(Login);
+});
+
+describe("Login", () => {
+  it("should render without errors", () => {
+    loginComponent = findByTestAttr(wrapper, "component-login");
+    expect(loginComponent).toHaveLength(1);
+  });
+
+  it("should contain a log in form", () => {
+    const loginForm = findByTestAttr(wrapper, "login-form");
+    expect(loginForm).toHaveLength(1);
+  });
+
+  it("should contain a submit button", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton).toHaveLength(1);
+  });
+});
