f1495d8123f86e04d31f497a882d3edc0fce03ec
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-05-11 16:57:42 +0100

    profile reducer availalbe

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,7 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
 import { Provider } from "react-redux";
-
 import store from "./store";
-import "./App.css";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
