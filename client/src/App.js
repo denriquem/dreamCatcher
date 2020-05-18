8794e2e6225f146643600079b20c424e0f85d3c8
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-05-18 18:38:00 +0100

    profile component beginings

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import Dashboard from "./components/dashboard/Dashboard";
 import EditProfile from "./components/profile-forms/EditProfile";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 import PrivateRoute from "./components/routing/PrivateRoute";
 
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
