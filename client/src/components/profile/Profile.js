5280fdf8bcd43c741c3a0d98a15457f522c20796
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-05-19 16:36:18 +0100

    profile with functioning edit button

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
-import { Link } from "react-router-dom";
 
 const Profile = ({
   getProfileById,
@@ -17,24 +17,28 @@ const Profile = ({
 
   return (
     <Fragment>
-      {" "}
-      profile
-      <Link to="/profiles" className="btn btn-light">
-        Back to Profiles
-      </Link>
-      {auth.isAuthenticated &&
-        auth.loading === false &&
-        auth.user._id === profile.user._id && (
-          <Link to="/edit-profile" className="btn btn-dark">
-            Edit Profile
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
           </Link>
-        )}
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
     </Fragment>
   );
 };
 
 Profile.propTypes = {
-  getProfileById: PropTypes.func.isRequired,
+  getProfileByID: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
 };
