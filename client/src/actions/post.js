976e075c9c683b798bed88a57ef9eac2556fdace
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-05-25 13:12:10 +0100

    feat: add like functionality complete

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,15 +1,49 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
   try {
-    const res = await axios.get("api/posts");
+    const res = await axios.get("/api/posts");
 
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
     });
   } catch (err) {
     dispatch({
